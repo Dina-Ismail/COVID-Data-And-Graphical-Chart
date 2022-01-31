0472864c0218579a6aff554e6a4e65b9859018af
Dina-I2020 <dinaelsadek@hotmail.com>
2022-01-31 15:01:24 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment1gc200485862 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment1gc200485862 to javafx.fxml;
+    exports com.example.assignment1gc200485862;
+}
