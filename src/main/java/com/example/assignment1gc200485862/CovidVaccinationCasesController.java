35192a41c4d541f1ee7be67cd4882fc386c5132d
Dina-I2020 <dinaelsadek@hotmail.com>
2022-02-17 17:55:03 -0500

    Updated the Controller and DB Utility to consider the radio button filtration

--- src/main/java/com/example/assignment1gc200485862/CovidVaccinationCasesController.java
+++ src/main/java/com/example/assignment1gc200485862/CovidVaccinationCasesController.java
@@ -1,10 +1,12 @@
 package com.example.assignment1gc200485862;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+
 
 import java.net.URL;
 import java.util.Date;
@@ -30,14 +32,73 @@ public class CovidVaccinationCasesController implements Initializable {
     @FXML
     private TableColumn<CovidVaccinationCases, Integer> totalNumbOfCasesColumn;
 
+    @FXML
+    private RadioButton allRecordsRadioBtn;
+
+    @FXML
+    private RadioButton year21RadioBtn;
+
+    @FXML
+    private RadioButton year22RadioBtn;
+
+    @FXML
+    private ToggleGroup yes;
+
+    @FXML
+    private Label selectionLabel;
+
+    @FXML
+    private Label totalRecordsLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         dateColumn.setCellValueFactory(new PropertyValueFactory<>("date")); //here it actually uses the get method of this variable
         unVaccinatedCasesColumn.setCellValueFactory(new PropertyValueFactory<>("unVaccinatedCases"));
         partialVaccinatedCasesColumn.setCellValueFactory(new PropertyValueFactory<>("partialVaccinatedCases"));
         vaccinatedCasesColumn.setCellValueFactory(new PropertyValueFactory<>("vaccinatedCases"));
         totalNumbOfCasesColumn.setCellValueFactory(new PropertyValueFactory<>("totalNumbOfCases"));
+//        allRecordsRadioBtn.setSelected(true);
+//        tableView.getItems().addAll(DBUtility.getCasesFromDB( DBUtility.createSQLStatement(1)));
+//        totalRecordsLabel.setText("Number of Records:" + DBUtility.counter);
 
-        tableView.getItems().addAll(DBUtility.getCasesFromDB());
-    }
+        yes.selectedToggleProperty().addListener(new ChangeListener<Toggle>()
+        {
+            public void changed(ObservableValue<? extends Toggle> ob,
+            Toggle o, Toggle n)
+
+            {
+
+                if(allRecordsRadioBtn.isSelected()) {
+                    tableView.getItems().removeAll();
+                    selectionLabel.setText("All Records are Shown");
+                    tableView.getItems().addAll(DBUtility.getCasesFromDB( DBUtility.createSQLStatement(1)));
+                    totalRecordsLabel.setText("Number of Records:" + DBUtility.counter);
+
+                }
+
+                 if(year21RadioBtn.isSelected())
+                {
+                    tableView.getItems().removeAll();
+                    selectionLabel.setText("Year 2021 Data Only");
+                    tableView.getItems().addAll(DBUtility.getCasesFromDB( DBUtility.createSQLStatement(2)));
+                    totalRecordsLabel.setText("Number of Records:" + DBUtility.counter);
+                    tableView.refresh();
+                }
+                else if(year22RadioBtn.isSelected())
+                {
+                    tableView.getItems().removeAll();
+                    selectionLabel.setText("Year 2022 Data Only");
+                    tableView.getItems().addAll(DBUtility.getCasesFromDB( DBUtility.createSQLStatement(3)));
+                    totalRecordsLabel.setText("Number of Records:" + DBUtility.counter);
+                }
+
+            }
+        });
+
+
+        }
+
+
+
 }
--- src/main/java/com/example/assignment1gc200485862/DBUtility.java
+++ src/main/java/com/example/assignment1gc200485862/DBUtility.java
@@ -8,22 +8,52 @@ public class DBUtility {
     private static String user = "Dina200485862";
     private static String password = "gjvSH68mVu";
     private static String connectURL = "jdbc:mysql://172.31.22.43:3306/Dina200485862";
+    public static int counter; // this counter to get the number of records in the resultset
 
+    /**
+     * This method returns the correct sql statement based on the chosen radio button
+     *
+     * @param i
+     */
+    public static String createSQLStatement(int i) {
+        String sql = "";
+        if (i == 1) {
+            sql = "Select Date, covid19_cases_unvac,covid19_cases_partial_vac,covid19_cases_full_vac, \n" +
+                    "    (covid19_cases_unvac + covid19_cases_partial_vac+ covid19_cases_full_vac)\n" +
+                    "    as 'totalCases' from COVID\n" +
+                    "    order by Date;";
+        } else if (i == 2) {
+            sql = "Select Date, covid19_cases_unvac,covid19_cases_partial_vac,covid19_cases_full_vac,\n" +
+                    "                    (covid19_cases_unvac + covid19_cases_partial_vac+ covid19_cases_full_vac)\n" +
+                    "                   as 'totalCases' from COVID\n" +
+                    "                   where year(date)=2021\n" +
+                    "                   order by Date;";
+        } else if (i == 3) {
+            sql = "Select Date, covid19_cases_unvac,covid19_cases_partial_vac,covid19_cases_full_vac,\n" +
+                    "                                    (covid19_cases_unvac + covid19_cases_partial_vac+ covid19_cases_full_vac)\n" +
+                    "                                      as 'totalCases' from COVID\n" +
+                    "                                     where year(date)=2022\n" +
+                    "                                     order by Date;";
+        }
+
+        return sql;
+    }
 
     /**
      * This method returns all COVID Cases information from the DB in an ArrayList
+     *
+     * @param sql
      */
-    public static ArrayList<CovidVaccinationCases> getCasesFromDB(){
+    public static ArrayList<CovidVaccinationCases> getCasesFromDB(String sql) {
         ArrayList<CovidVaccinationCases> covidCases = new ArrayList<>(); // it creates by container to hold the data from database
-        String sql = "Select Date, covid19_cases_unvac,covid19_cases_partial_vac,covid19_cases_full_vac, \n" +
-                "    (covid19_cases_unvac + covid19_cases_partial_vac+ covid19_cases_full_vac)\n" +
-                "    as 'totalCases' from COVID\n" +
-                "    order by Date;";
         try (
                 Connection conn = DriverManager.getConnection(connectURL, user, password);
                 Statement statement = conn.createStatement();
                 ResultSet resultSet = statement.executeQuery(sql);
         ) {
+            //starting the counter from 0 everytime the getCasesFromDB method is called
+            counter = 0;
+            covidCases.removeAll(covidCases);
             while (resultSet.next()) //loop around the resultSet
             {// create objects from CovidVaccinationCases class by assigning the attributes to the db columns
                 Date date = resultSet.getDate("Date");
@@ -31,20 +61,19 @@ public class DBUtility {
                 int partialVaccinatedCases = resultSet.getInt("covid19_cases_partial_vac");
                 int vaccinatedCases = resultSet.getInt("covid19_cases_full_vac");
                 int total = resultSet.getInt("totalCases");
-
-                System.out.println("");
                 //creating an object based on the data pulled from database
-                CovidVaccinationCases cases = new CovidVaccinationCases(date,unVaccinatedCases,partialVaccinatedCases,vaccinatedCases,total);
+                CovidVaccinationCases cases = new CovidVaccinationCases(date, unVaccinatedCases, partialVaccinatedCases, vaccinatedCases, total);
                 // I am adding each record pulled to my arrayList
                 covidCases.add(cases);
+                counter++;
             }
-        } catch (SQLException e) {
+         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-
         return covidCases;
-
     }//end of getCasesFromDB()
+
+
+
 }
 
